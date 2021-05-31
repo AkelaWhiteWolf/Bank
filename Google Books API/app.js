2076e3927e3f4ccb773c7dca60072a446da95f5c
Alexey Shvets <alexey199822la@gmail.com>
2021-05-31 21:06:00 +0300

    /work is going/

--- Google Books API/app.js
+++ Google Books API/app.js
@@ -1,12 +1,20 @@
 'use strict';
+
+const button = document.querySelector('button');
+const input = document.querySelector('input');
+
+button.addEventListener('click', (e) => {
+    e.preventDefault();
+    let search = input.value;
+    getBooks(search);
+});
+
 async function getBooks(search) {
     let request = await fetch(`https://www.googleapis.com/books/v1/volumes?q=${search}+inauthor:keyes`);
     if (request.ok) {
-        let result = await request.json();
-        console.log(result);
-    } else {``
+        let searchResults = await request.json();
+        console.log(searchResults);
+    } else {
         return new Error('Something went wrong');
     }
 }
-
-getBooks('Цветы для элджернона');
