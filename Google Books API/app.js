1e7e1f58185a39f8ae876ab3114fba055fbf7745
Alexey Shvets <alexey199822la@gmail.com>
2021-06-01 21:08:09 +0300

    /work is going/

--- Google Books API/app.js
+++ Google Books API/app.js
@@ -3,18 +3,25 @@
 const button = document.querySelector('button');
 const input = document.querySelector('input');
 
-button.addEventListener('click', (e) => {
-    e.preventDefault();
-    let search = input.value;
-    getBooks(search);
-});
+class Book {
+    constructor(searchText) {
+        this.searchText = searchText;
+    }
 
-async function getBooks(search) {
-    let request = await fetch(`https://www.googleapis.com/books/v1/volumes?q=${search}+inauthor:keyes`);
-    if (request.ok) {
-        let searchResults = await request.json();
-        console.log(searchResults);
-    } else {
-        return new Error('Something went wrong');
+    async showBooks() {
+        let request = await fetch(`https://www.googleapis.com/books/v1/volumes?q=${this.searchText}+inauthor:keyes`);
+        if (request.ok) {
+            let searchResults = await request.json();
+            console.log(searchResults);
+        } else {
+            return new Error('Something went wrong');
+        }    
     }
 }
+
+button.addEventListener('click', (e) => {
+    e.preventDefault();
+    let searchText = input.value;
+    let result = new Book(searchText);
+    result.showBooks();
+});
