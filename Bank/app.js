db550bdf4da7720d3ca681c4d09cf9bf8db80520
Alexey Shvets <alexey199822la@gmail.com>
2021-06-16 22:19:47 +0300

    created button to add new bank, started working on modal window

--- Bank/app.js
+++ Bank/app.js
@@ -1,5 +1,6 @@
-let mono = document.querySelector('.bank');
-mono.removeAttribute('data-invisible');
-mono.addEventListener('click', (e) => {
-    console.log(e.target.matches('.bank__button-add'));
-});
+(() => {
+    'use strict';
+
+    
+
+})();
