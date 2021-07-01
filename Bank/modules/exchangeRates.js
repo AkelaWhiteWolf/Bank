9e5567bbec0225c3697fbd4329ddd693fc95b059
Alexey Shvets <alexey199822la@gmail.com>
2021-07-01 22:37:32 +0300

    work is going

--- Bank/modules/exchangeRates.js
+++ Bank/modules/exchangeRates.js
@@ -1,6 +1,19 @@
 export let exchangeRates = () => {
     'use strict';
 
+    function showDate() {
+        // const formCalendar = document.querySelector('.form-calendar');
+        const calendar = document.querySelector('#calendar');
+        const btnCalendar = document.querySelector('#btn-calendar');
+
+        btnCalendar.addEventListener('click', () => {
+            console.log(calendar.value);
+            calendar.value = '';
+        });
+
+    }
+    showDate();
+
     function takeDate(date = new Date()) {
         let year = String(date.getFullYear());
         let month = String(date.getMonth() + 1);
@@ -16,17 +29,15 @@ export let exchangeRates = () => {
         return `${year}${month}${day}`;
     }
 
-    async function getExchange(date, currency = '') {
-        let response = await fetch(`https://bank.gov.ua/NBUStatService/v1/statdirectory/exchangenew?valcode=${currency}&date=${date}&json`);
-        console.log(response.status);
-        let json = await response.json();
-        console.log(json);
-
-        // try {
-        // } catch(error) {
-        //     console.error(error);
-        // }
+    async function getExchange(date, currency) {
+        try {
+            let response = await fetch(`https://bank.gov.ua/NBUStatService/v1/statdirectory/exchangenew?valcode=${currency}&date=${date}&json`);
+            let json = await response.json();
+            return json;
+        } catch(error) {
+            console.error(error);
+        }
     }
 
-    getExchange(takeDate(), 'EUR');
+    // getExchange(takeDate(), 'EUR');
 };
