3206b53b42c542df5ba6cf5f6b5a908ce6115ecf
Alexey Shvets <alexey199822la@GMAIL.COM>
2021-07-08 16:57:14 +0300

    work is going

--- Bank/modules/exchangeRatesModules.js
+++ Bank/modules/exchangeRatesModules.js
@@ -1,28 +1,45 @@
 export let exchangeRatesModules = () => {
     'use strict';
 
-    // function showDate() {
-    //     const calendar = document.querySelector('#calendar');
-    //     const btnCalendar = document.querySelector('#btn-calendar');
-
-    //     btnCalendar.addEventListener('click', () => {
-    //         console.log(calendar.value);
-    //         calendar.value = '';
-    //     });
-    // }
-
     const btnFlipLeft = document.querySelector('.btn-carousel[data-flip="left"]');
     const btnFlipRight = document.querySelector('.btn-carousel[data-flip="right"]');
+    const content = document.querySelector('.carousel-content');
+    let carouselElements = document.querySelectorAll('.carousel-content__elem');
 
-    btnFlipLeft.addEventListener('click', () => flipCarousel('left'));
-    btnFlipRight.addEventListener('click', () => flipCarousel('right'));
+    function loopCarousel() {
+        let newCarousel = [];
+
+        for (let e of carouselElements) {
+            let newElem = e.cloneNode(true);
+            newCarousel.push(newElem);
+        }
+        console.log(newCarousel);
+        
+        for (let i = newCarousel.length - 1; i > 0; i--) {
+            content.prepend(newCarousel[i]);
+        }
+        // if (side === 'left') {
+        //     for (let e of newCarousel) {
+        //         content.prepend(e);
+        //     }
+        // } else {
+        //     for (let e of newCarousel) {
+        //         content.append(e);
+        //     }
+        // }
+        
+    }
+
+    loopCarousel();
+
+    // btnFlipLeft.addEventListener('click', () => flipCarousel('left'));
+    // btnFlipRight.addEventListener('click', () => flipCarousel('right'));
 
     function flipCarousel(route) {
-        const content = document.querySelector('.carousel-content');
-        const carouselElem = document.querySelector('.carousel-content__elem');
-        let currentMargin = carouselElem.style.marginLeft;
+        let carouselFirstElem = carouselElements[0];
+        let currentMargin = carouselFirstElem.style.marginLeft;
 
-        if (!currentMargin || currentMargin === '0%' || currentMargin === '') {
+        if (!currentMargin || currentMargin === '0%') {
             currentMargin = 0;
         } else {
             currentMargin = currentMargin.slice(0, currentMargin.length - 1);
@@ -31,55 +48,53 @@ export let exchangeRatesModules = () => {
         if (route === 'left') {
             if (currentMargin == 0) {
                 content.style.flexDirection = 'row-reverse';
-                console.log(carouselElem.style.marginLeft);
-                carouselElem.style.marginRight = `${Number(currentMargin) - 100}%`;
+                console.log(carouselFirstElem.style.marginLeft);
+                carouselFirstElem.style.marginRight = `${Number(currentMargin) - 100}%`;
             } else {
-                carouselElem.style.marginLeft = `${Number(currentMargin) + 100}%`;
+                carouselFirstElem.style.marginLeft = `${Number(currentMargin) + 100}%`;
             }
         } else {
             if (currentMargin == -500) {
-                carouselElem.style.marginLeft = '';
-                console.log(carouselElem.style.marginLeft);
+                carouselFirstElem.style.marginLeft = '';
+                console.log(carouselFirstElem.style.marginLeft);
             }
-            carouselElem.style.marginLeft = `${currentMargin - 100}%`;
+            carouselFirstElem.style.marginLeft = `${currentMargin - 100}%`;
         }
     }
     
-    let exchangeRates = {
-        
-    };
     
+
+
     
-    
-    function convertDate(date = new Date()) {
-        let year = String(date.getFullYear());
-        let month = String(date.getMonth() + 1);
-        let day = String(date.getDate());
+    // function convertDate(date = new Date()) {
+    //     let year = String(date.getFullYear());
+    //     let month = String(date.getMonth() + 1);
+    //     let day = String(date.getDate());
 
-        if (month.length < 2) {
-            month = `0${month}`;
-        }
-        if (day.length < 2) {
-            day = `0${day}`;
-        }
+    //     if (month.length < 2) {
+    //         month = `0${month}`;
+    //     }
+    //     if (day.length < 2) {
+    //         day = `0${day}`;
+    //     }
 
-        return `${year}${month}${day}`;
-    }
+    //     return `${year}${month}${day}`;
+    // }
 
-    async function getExchange(currency, date = '') {
-        try {
-            let response = await fetch(`https://bank.gov.ua/NBUStatService/v1/statdirectory/exchangenew?valcode=${currency}&date=${date}&json`);
-            let json = await response.json();
-            return json;
-        } catch(error) {
-            console.error(error);
-        }
-    }
+    // async function getExchange(currency, date = '') {
+    //     try {
+    //         let response = await fetch(`https://bank.gov.ua/NBUStatService/v1/statdirectory/exchangenew?valcode=${currency}&date=${date}&json`);
+    //         let json = await response.json();
+    //         return json;
+    //     } catch(error) {
+    //         console.error(error);
+    //     }
+    // }
 
-    async function createCarousel() {
-        const currencyArr = document.querySelectorAll('.carousel-content__elem');
-        for (let elem of currencyArr) {
-            let rate = elem.querySelector('.rate');
-        }
-    }
+    // async function createCarousel() {
+    //     const currencyArr = document.querySelectorAll('.carousel-content__elem');
+    //     for (let elem of currencyArr) {
+    //         let rate = elem.querySelector('.rate');
+    //     }
+    // }
 };
